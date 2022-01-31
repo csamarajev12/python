ddbd79692f07b390047676b48ada7f524ee354be
csamarajev12 <98739412+csamarajev12@users.noreply.github.com>
2022-01-31 11:35:05 +0200

    Add files via upload

---
+++ yl1.py
@@ -0,0 +1,50 @@
+# Harjutus 01
+# Caroline Samarajev
+# 31.01.2022
+
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+print ("  @..@")
+print (" (----)")
+print("( \__/ )")
+print('^^ "" ^^')
+
+a = int(input("Sisesta ruudu külje pikkus: "))
+s = a * a
+print("Vastus: ruudu pindala on",s,"cm^2")
+
+
+
+'''
+nimi = input("Lisa oma nimi: ")
+print("Tere", nimi+"!") #pluss ei lisa tyhikut
+vanus = int(input("Lisa oma vanus: "))
+vanuste_vahe = 17-vanus
+print("Meie vanuste vahe on:", vanuste_vahe, "aastat")
+'''
+
+
+
+'''
+a = 12
+b = 16
+a = b
+s=a*b
+p = 2*(a+b)
+print("Pindala",s,"ruutmeetrit")
+print("Ümbermõõt",p,"meetrit")
+'''
+
+
+
+
+
+'''
+print("Tere maailm") #sõne
+print('Tere sa ka') #String
+print(2000) #int / täisarv
+print(2.5) #float / komaarv
+print('Ütle "Tere Maailm"')
+print("Ütle \"Tere Maailm\"") #escape string
+print(type(2000)) #näitab andmetüüpi
+'''
